5457abeafc86970797d4bbf36d51c345c08979fe
marioabal <85706843+marioabal@users.noreply.github.com>
2021-06-16 02:02:34 -0600

    Agregar archivos de proyecto.

---
+++ Estructura de datos tipo vector/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Estructura_de_datos_tipo_vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] dato = new char[10];
+            char buscar;
+
+            //dato[0] = 'a';
+
+            for (int i=0; i<=9; i++)
+            {
+                Console.WriteLine("i: " + i);
+                Console.Write("Ingrese una letra: ");
+                dato[i] = char.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("-------------------");
+
+            Console.Write("Ingrese caracter a buscar: ");
+            buscar = char.Parse(Console.ReadLine());
+            {
+                if (dato[10] == buscar);
+                {
+                    Console.Write("dato " + buscar );
+                }
+            }
+        }
+    }
+}
